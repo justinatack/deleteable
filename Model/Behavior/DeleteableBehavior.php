bd1d24d7e2e8f73567ebb10a41d47893ed7f21dd
Justin Atack <justinatack@gmail.com>
2015-01-08 22:08:29 +1100

    Initial commit

---
+++ Model/Behavior/DeleteableBehavior.php
@@ -0,0 +1,14 @@
+<?php
+
+class DeleteableBehavior extends ModelBehavior {
+
+/**
+ * Configuration of model
+ *
+ * @param Model $Model
+ * @param array $config
+ * @return void
+ */
+	public function setup(Model $Model, $config = array()) {
+	}
+}
