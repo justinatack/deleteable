d11a6c30a505ab81d33dc47df0326de27728d3a0
Justin Atack <justinatack@gmail.com>
2015-01-09 15:22:17 +1100

    Bug fixes
    Changed the $find method slightly as per Jose Gonzales

--- Model/Behavior/DeleteableBehavior.php
+++ Model/Behavior/DeleteableBehavior.php
@@ -1,5 +1,22 @@
 <?php
-App::uses('ModelBehavior', 'Model');
+/**
+ * Copyright 2015 Justin Atack (http://www.justinatack.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2015 Justin Atack (http://www.justinatack.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * Deleteable Plugin
+ *
+ * Deleteable Behavior
+ *
+ * @package Deleteable
+ * @subpackage deletable.models.behaviors
+ */
 class DeleteableBehavior extends ModelBehavior
 {
 
@@ -24,21 +41,33 @@ class DeleteableBehavior extends ModelBehavior
         );
     }
 
-    public function isDeleteable($Model) {
+    /**
+     * isDeleteable
+     * @param Model  $Model
+     * @return boolean
+     */
+    public function isDeleteable($Model)
+    {
         $boolean = $this->settings[$Model->alias]['boolean'];
         $field = $this->settings[$Model->alias]['field'];
 
-        $find = $Model->findById($Model->id);
+        $find = $Model->find('first', ['conditions' => [$Model->primaryKey => $Model->id]]);
 
-        if ($find[$Model->alias][$field] == $boolean) {
-            return false;
+        if (isset($find[$Model->alias][$field])) {
+            return $find[$Model->alias][$field] != $boolean;
         }
 
         return true;
+
     }
 
-
-    public function beforeDelete(Model $Model, $cascade = false)
+    /**
+     * beforeDelete
+     * @param  Model   $Model
+     * @param  boolean $cascade
+     * @return boolean
+     */
+    public function beforeDelete(Model $Model, $cascade = true)
     {
         return $this->isDeleteable($Model);
     }
