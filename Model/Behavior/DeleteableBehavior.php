0c3f1976c188d30af0fcade97b24d2a681ecc949
Justin Atack <justinatack@gmail.com>
2015-01-09 13:58:09 +1100

    Added DeleteableBehavior.php, composer.json and License.txt

--- Model/Behavior/DeleteableBehavior.php
+++ Model/Behavior/DeleteableBehavior.php
@@ -1,14 +1,45 @@
 <?php
+App::uses('ModelBehavior', 'Model');
+class DeleteableBehavior extends ModelBehavior
+{
 
-class DeleteableBehavior extends ModelBehavior {
+    /**
+     * Configuration of model
+     *
+     * @param Model $Model
+     * @param array $settings
+     * @return void
+     */
+    public function setup(Model $Model, $settings = array())
+    {
+        if (!isset($this->settings[$Model->alias])) {
+            $this->settings[$Model->alias] = array(
+                'field' => 'delete',
+                'boolean' => false
+            );
+        }
+        $this->settings[$Model->alias] = array_merge(
+            $this->settings[$Model->alias],
+            (array)$settings
+        );
+    }
 
-/**
- * Configuration of model
- *
- * @param Model $Model
- * @param array $config
- * @return void
- */
-	public function setup(Model $Model, $config = array()) {
-	}
+    public function isDeleteable($Model) {
+        $boolean = $this->settings[$Model->alias]['boolean'];
+        $field = $this->settings[$Model->alias]['field'];
+
+        $find = $Model->findById($Model->id);
+
+        if ($find[$Model->alias][$field] == $boolean) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public function beforeDelete(Model $Model, $cascade = false)
+    {
+        return $this->isDeleteable($Model);
+    }
 }
